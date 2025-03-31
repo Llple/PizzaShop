24f91460e5473bbb645df5d61065a2b97941c4c3
Llplee <you@example.com>
2025-03-31 14:40:34 +0300

    New comite 1

--- src/page/NotFound.tsx
+++ src/page/NotFound.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 import NotFoundBlock from "../NotFoundBlock/index"
+import { useNavigate } from 'react-router'
+
 
 export default function NotFound() {
+  const navigate = useNavigate()
+  React.useEffect(()=>{
+    setTimeout(()=>{
+      navigate("/")
+    },200)
+  })
   return (
     <div><NotFoundBlock/></div>
   )
